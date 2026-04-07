8845f92acbaf63a25cea42a1c4009d1f970a6823
Camak2441 <131969191+Camak2441@users.noreply.github.com>
2026-04-07 21:59:47 +0100

    Fixed bug in parsing clamp arguments

--- examples/scene_args_loader.py
+++ examples/scene_args_loader.py
@@ -172,9 +172,9 @@ def process_config(cfg):
                     else:
                         args.append(f"t{cfg.texture_width}x{cfg.texture_height}")
                 if cfg.textured_rgb_clamp != "clamp":
-                    args.append(f"rgb{cfg.rgb_clamp}")
+                    args.append(f"rgb{cfg.textured_rgb_clamp}")
                 if cfg.textured_alpha_clamp != "normalize":
-                    args.append(f"a{cfg.alpha_clamp}")
+                    args.append(f"a{cfg.textured_alpha_clamp}")
                 args_suffix = "_".join(args)
                 if len(args_suffix) != 0 and args_suffix[0] != "_":
                     args_suffix = "_" + args_suffix
