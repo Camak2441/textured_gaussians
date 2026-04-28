c5f949300c4fc9a20cf8d3bdfedd86a22f19e621
Camak2441 <131969191+Camak2441@users.noreply.github.com>
2026-04-28 18:58:01 +0100

    Added more scripts

---
+++ scripts/train/2dgs/g2000_nerf_synthetic.sh
@@ -0,0 +1,8 @@
+bash train/2dgs/g2000.sh chair
+bash train/2dgs/g2000.sh drums
+bash train/2dgs/g2000.sh ficus
+bash train/2dgs/g2000.sh hotdog
+bash train/2dgs/g2000.sh lego
+bash train/2dgs/g2000.sh materials
+bash train/2dgs/g2000.sh mic
+bash train/2dgs/g2000.sh ship
